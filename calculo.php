8f89b880102682c4abda8a917219a29f1fb0803d
GabrielTSR <gabrielalvesrocha22@gmail.com>
2021-08-12 19:00:59 -0300

    estilizacao feita

--- calculo.php
+++ calculo.php
@@ -1,3 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;1,300&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+    <title>Valor do aumento</title>
+</head>
+
 <?php
 
 if(isset($_POST['nome']) &&
@@ -15,7 +28,7 @@ isset($_POST['cargo'])
         $salarioNovo = ($salarioAntigo / 100 * 10) + $salarioAntigo;
 
     } else {
-        $salarioNovo = ($salarioAntigo / 100 * 20) + $salarioAntigo;
+        $salarioNovo = number_format(($salarioAntigo / 100 * 20) + $salarioAntigo, 2, ',', '.');
     }
 
     if($genero == 'masculino') {
@@ -26,28 +39,24 @@ isset($_POST['cargo'])
         $theGenero = 'I';
     }
 
-    echo"$theGenero $nome passará a receber R$$salarioNovo, no cargo de $cargo";
+    $mensagem = "$theGenero $nome passará a receber R$<span class=\"novoSalario\">$salarioNovo</span>, no cargo de $cargo.";
 
 
 
 
 } else {
-    echo"<h1>Você não enviou as informações corretamente</h1>";
+    
+    echo"<h1 id=\"error-message\">As informações não foram enviadas corretamente</h1>";
     exit;
 }
 
 ?>
 
-<!DOCTYPE html>
-<html lang="pt-BR">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>Valor do aumento</title>
-</head>
+
 <body>
-    <!-- <?=$mensagem?> -->
+    <div id="circle-container">
+        <h1>RESULTADO:</h1>
+        <p><?=$mensagem?></p>
+    </div>
 </body>
 </html>
